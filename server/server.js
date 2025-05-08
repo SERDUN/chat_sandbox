7a2597e040caa1b6d0bc830e690795b2c43c53c2
Dmitro Serdun <d.serdun@webtrit.com>
2025-05-09 00:15:28 +0300

    feat: add basic browser-based version of chat client

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const fs = require('fs');
+import express from 'express';
+import cors from 'cors';
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(cors({
+  origin: 'https://chat-sanbox-web.fly.dev'
+}));
 
 const users = new Map(); // userId => { name, lastSeen }
 const messages = [];     // { userId, text, timestamp }
